8b48066622f947d4ea74ab99216b6eecab9179dc
souravinfomaticae <151753410+souravinfomaticae@users.noreply.github.com>
2023-12-11 10:49:37 +0530

    home , signup , signup page created and develop routing

---
+++ src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import Navbar from "../features/Navbar/Navbar";
+import ProductList from "../product-list/ProductList";
+const Home = () => {
+  return (
+    <div>
+      <Navbar>
+       <ProductList></ProductList>
+      </Navbar>
+    </div>
+  )
+}
+
+export default Home
